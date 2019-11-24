022f346d08f9da5c3659244dd6006aa54d0f4a69
hongchenhao <31385593+1046329594@users.noreply.github.com>
2019-11-24 23:21:03 +0800

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 echo "http://$FROM {
 gzip
-proxy / http://$TO
+proxy / http://$TO {
+    header_upstream Host {host}
+    header_upstream X-Real-IP {remote}
+    header_upstream X-Forwarded-For {remote}
+    header_upstream X-Forwarded-Proto {scheme}
+    }
 }" > /usr/local/caddy/Caddyfile
 caddy --conf /usr/local/caddy/Caddyfile
